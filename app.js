e9d2ddb0af1fe8fbf10bb713e19007e8bfdc223b
Sunbum Kwon <ksunbum97@gmail.com>
2022-07-27 16:39:01 +0900

    cloneCode_모멘텀(연습)

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-const player = {
-    Name : 'Sunbum',
-    Dead : false,
-    Age : 26
-};
+document.title = "Cloned 모멘텀"
+let head =  'Got you!'
 
-if (!player.Dead) {
-    console.log('player is dead TWT')
-    player.Dead = "Sunbum is dead.."
-    player.Age = player.Age + 400
-    console.log(player)
-}
-else{
-    player.lastName = "kwon"
-    player.Dead = "Sunbum is alive!"
-    console.log(player)
-}
+document.getElementById("header1").textContent = head
+console.log(head)
