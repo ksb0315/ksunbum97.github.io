99364de93276ce3fdbfecab74aa3c73066fc9a38
Sunbum Kwon <ksunbum97@gmail.com>
2022-07-29 14:28:20 +0900

    cloneCode_모멘텀(연습3)

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
-console.dir(h1)
 function handleh1Click() {
-    h1.style.color = "blue";
-}
-
-function mouseEnter() {
-    h1.innerText = "mouse is here";
-}
-
-function mouseLeave() {
-    h1.innerText = "mouse leaved..";
-    h1.style.color = "yellow";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!")
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("WIFI is back!");
+    const cur_col = h1.style.color;
+    let newCol;
+    console.log(cur_col);
+    if (cur_col === "blue") {
+        newCol = "tomato";
+    }
+    else {
+        newCol = "blue";
+    }
+    h1.style.color = newCol;
 }
 
 h1.addEventListener("click", handleh1Click)
-h1.addEventListener("mouseenter", mouseEnter)
-h1.addEventListener("mouseleave", mouseLeave)
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
